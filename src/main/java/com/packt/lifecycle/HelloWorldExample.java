93b0becf58e65d097d61676e8e1a56d6beae74d1
k0reander <k0reander@yahoo.com>
2018-01-16 13:37:04 +0100

    removed hooks like BeanAware, FactoryAware etc...

--- src/main/java/com/packt/lifecycle/HelloWorldExample.java
+++ src/main/java/com/packt/lifecycle/HelloWorldExample.java
@@ -1,15 +1,13 @@
 package com.packt.lifecycle;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		context.registerShutdownHook();
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		HelloWorld world = (HelloWorld) context.getBean("helloWorld");
 		System.out.println(world.getMessage());
 	}
