455859d1cf2f521046f6bbc20447bc37698b07d2
Frank Dupont <k0reander@yahoo.com>
2018-01-30 14:04:17 +0100

    improved readability PhoneEntryTest

--- src/test/java/com/packt/jdbc/PhoneEntryTest.java
+++ src/test/java/com/packt/jdbc/PhoneEntryTest.java
@@ -8,11 +8,11 @@ public class PhoneEntryTest {
 
 	@Test
 	public void testtoString() {
-		String expected = "Frank Dupont : 0478975011";
 		PhoneEntry entry = new PhoneEntry();
 		entry.setFirstName("Frank");
 		entry.setLastName("Dupont");
 		entry.setPhoneNumber("0478975011");
+		String expected = "Frank Dupont : 0478975011";
 		assertEquals("toString() method incorrect", expected, entry.toString());
 	}
 
