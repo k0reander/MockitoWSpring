9c01db22bb19c9240adeff5b8fbc55c062708ef1
Frank Dupont <k0reander@yahoo.com>
2018-01-31 18:34:22 +0100

    created trading model classes

--- src/main/java/com/packt/trading/Portfolio.java
+++ src/main/java/com/packt/trading/Portfolio.java
@@ -1,4 +1,49 @@
 package com.packt.trading;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+import com.packt.trading.dto.Stock;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Optional;
+
 public class Portfolio {
+    private Multimap<String, Stock> portfolio = ArrayListMultimap.create();
+
+    public void buy(Stock stock){
+        portfolio.put(stock.getSymbol(), stock);
+    }
+
+
+    public void sell(Stock stock, int quantity){
+        if(quantity < 0)
+            throw new IllegalArgumentException("Cannot sell negative amount fo stock");
+
+        portfolio.get(stock.getSymbol()).stream()
+                .limit(quantity)
+                .forEach( s -> portfolio.remove(s.getSymbol(), s));
+    }
+
+
+    public BigDecimal getAvgPrice(Stock stock){
+        Collection<Stock> stocks = portfolio.get(stock.getSymbol());
+
+        Optional<BigDecimal> avgPriceOptional = stocks.stream()
+                .map(Stock::getPrice)
+                .reduce(BigDecimal::add);
+
+        if(!avgPriceOptional.isPresent())
+            throw new IllegalArgumentException("Cannot average price of stock not in portfolio : " + stock);
+        else
+            return avgPriceOptional.get().divide(new BigDecimal( stocks.size() ));
+    }
+
+
+    public BigDecimal getCurrentValue() {
+        BigDecimal avgPrice = BigDecimal.ZERO;
+        return portfolio.values().stream()
+                .map(c -> c.getPrice())
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
 }
--- src/main/java/com/packt/trading/StockBroker.java
+++ src/main/java/com/packt/trading/StockBroker.java
@@ -1,4 +1,29 @@
 package com.packt.trading;
 
+import com.packt.trading.dto.Stock;
+
+import java.math.BigDecimal;
+
 public class StockBroker {
+
+    private final static BigDecimal LIMIT = new BigDecimal(0.10);
+
+    private MarketWatcher watcher;
+
+    public StockBroker(MarketWatcher watcher) {
+        this.watcher = watcher;
+    }
+
+    public void perform(Portfolio portfolio, Stock stock){
+        //if percetage gained of live stock is higher than a limit, sell 10
+        //if negative, than buy 1
+        Stock liveStock = watcher.getQuote(stock.getSymbol());
+        BigDecimal avg = portfolio.getAvgPrice(stock);
+        BigDecimal priceGained = liveStock.getPrice().subtract(avg);
+        BigDecimal percentGained = priceGained.divide(avg);
+        if( percentGained.compareTo(LIMIT) > 0 )
+            portfolio.sell(stock, 10);
+        if ( percentGained.compareTo(LIMIT) < 0)
+            portfolio.buy(stock);
+    }
 }
