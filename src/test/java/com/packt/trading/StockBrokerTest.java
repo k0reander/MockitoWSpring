c2c4fad96cd20bc7d93defa0d6d38b5708958971
Frank Dupont <k0reander@yahoo.com>
2018-02-01 11:33:04 +0100

    Running StockBrokerTest with MockitoJunitRunner

--- src/test/java/com/packt/trading/StockBrokerTest.java
+++ src/test/java/com/packt/trading/StockBrokerTest.java
@@ -1,24 +1,18 @@
 package com.packt.trading;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
 
+@RunWith(MockitoJUnitRunner.class)
 public class StockBrokerTest {
 
     @Mock
     MarketWatcher watcher;
-
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-    }
 
     @Test
     public void testSanity(){
