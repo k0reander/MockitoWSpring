975ce1a39672f38ce094ccd3445000bcb39b2b31
Frank Dupont <k0reander@yahoo.com>
2018-02-01 11:06:11 +0100

    created StockBrokerTest

--- src/test/java/com/packt/trading/StockBrokerTest.java
+++ src/test/java/com/packt/trading/StockBrokerTest.java
@@ -1,5 +1,34 @@
-import static org.junit.Assert.*;
+package com.packt.trading;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
 
 public class StockBrokerTest {
+
+    @Mock
+    MarketWatcher watcher;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testSanity(){
+        assertNotNull(watcher);
+    }
+
+    @Test
+    public void testPerform(){
+        Portfolio portfolio = mock(Portfolio.class);
+        throw new UnsupportedOperationException("To implement");
+    }
 
 }
