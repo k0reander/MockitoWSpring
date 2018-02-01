6e4e6132a6b9849e7a000dd85bfb0e2a0f066047
Frank Dupont <k0reander@yahoo.com>
2018-02-01 15:07:52 +0100

    tested perform() in StockBroker

--- src/test/java/com/packt/trading/StockBrokerTest.java
+++ src/test/java/com/packt/trading/StockBrokerTest.java
@@ -1,12 +1,18 @@
 package com.packt.trading;
 
+import com.packt.trading.dto.Stock;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.math.BigDecimal;
+
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StockBrokerTest {
@@ -14,15 +20,56 @@ public class StockBrokerTest {
     @Mock
     MarketWatcher watcher;
 
-    @Test
-    public void testSanity(){
-        assertNotNull(watcher);
+    StockBroker broker;
+
+    Stock myStock = new Stock("Stock", "A Comp", new BigDecimal(10) );
+    Stock highStock = new Stock("Stock", "A Comp", new BigDecimal(11.20) );
+    Stock lowStock = new Stock("Stock", "A Comp", new BigDecimal(9) );
+    Stock equalStock = new Stock("Stock", "A Comp", new BigDecimal(10) );
+
+    @Before
+    public void initBroker(){
+        broker = new StockBroker(watcher);
     }
 
     @Test
-    public void testPerform(){
+    public void testSanity(){
+        assertNotNull(watcher);
+        assertNotNull(broker);
+    }
+
+    @Test
+    public void test10SoldWhenStockHigh(){
         Portfolio portfolio = mock(Portfolio.class);
-        throw new UnsupportedOperationException("To implement");
+        when( portfolio.getAvgPrice( any(Stock.class) )).thenReturn( new BigDecimal(10.0) );
+        when( watcher.getQuote( anyString() )).thenReturn(highStock);
+        broker.perform(portfolio, myStock);
+        verify(portfolio).sell(myStock, 10);
+    }
+
+    @Test
+    public void bought1WhenStockLow(){
+        Portfolio portfolio = mock(Portfolio.class);
+        when( portfolio.getAvgPrice( any(Stock.class)) ).thenReturn( new BigDecimal(10.0) );
+        when( watcher.getQuote(anyString()) ).thenReturn(lowStock);
+        broker.perform(portfolio, myStock);
+        verify(portfolio).buy(myStock);
+    }
+
+    @Test
+    public void nothingHappensWhenStockUnchanged(){
+        Portfolio portfolio = mock(Portfolio.class);
+        when( portfolio.getAvgPrice( any(Stock.class) )).thenReturn(new BigDecimal(10.0));
+        when( watcher.getQuote(anyString()) ).thenReturn(equalStock);
+        broker.perform(portfolio, myStock);
+
+        //verifies that absolutely nothing else happens on portfolio after the initial call; fragile and depends on portfolio implementation
+        verify(portfolio).getAvgPrice(myStock);
+        verifyNoMoreInteractions(portfolio);
+
+        //verifies instead clearly that nothing is sold or bought; better solution
+        verify(portfolio, never()).buy( any(Stock.class) );
+        verify(portfolio, never()).sell( any(Stock.class), anyInt() );
     }
 
 }
